f1441919f1a71be4d17c542bcefcf69ce89e8e39
Isabella Reiff <izzyreiff@gmail.com>
2017-10-09 01:58:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getDataFromApi(){
             if (length > 8){
                 length = 8;
             }
-            finalHTML +=   `<div class="center-align"><h4>Showing ${length} Results From ${json.location.city_name}</h4></div>
+            finalHTML +=   `<div class="center-align"><h4><small>Showing ${length} Results From ${json.location.city_name}</small></h4></div>
 <br><br>`
                 for (var i = 0; i < length; i++){
                     finalHTML +=   `
